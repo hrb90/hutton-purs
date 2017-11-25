2efbc5b575d237d209539791a3d6454e1447c8db
hrb90 <harrisonrbrown90@gmail.com>
2017-11-25 10:50:14 -0500

    Working (?) parser with Sparkle frontend

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,16 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Control.Monad.Eff (Eff)
+import DOM (DOM)
+import Data.Bifunctor (lmap)
+import Data.Hutton (eval)
+import Parser (parseHutton)
+import Signal.Channel (CHANNEL)
+import Sparkle (sparkle)
+import Text.Parsing.Parser (runParser)
+
+main :: forall eff. Eff (channel :: CHANNEL, dom :: DOM | eff) Unit
+main = sparkle "Hutton's razor" parseAndEval where
+  parseAndEval = (lmap $ show) <$> (flip runParser) (eval <$> parseHutton)
---
+++ src/Parser.purs
@@ -0,0 +1,30 @@
+module Parser (parseHutton) where
+  
+import Prelude
+
+import Data.Array (some, reverse, uncons)
+import Data.Hutton (Expr(..))
+import Data.Maybe (Maybe(..))
+import Text.Parsing.Parser (Parser)
+import Text.Parsing.Parser.Expr (Assoc(..), Operator(..), buildExprParser)
+import Text.Parsing.Parser.String (string)
+import Text.Parsing.Parser.Token (digit)
+
+decimal :: Parser String Expr
+decimal = (Lit <<< toInt <<< reverse) <$> (some digit)
+  where toInt arr = case (uncons arr) of
+                        Just { head: x, tail: xs } -> charToInt x + 10 * toInt xs
+                        Nothing -> 0
+        charToInt x = case x of
+                        '1' -> 1
+                        '2' -> 2
+                        '3' -> 3
+                        '4' -> 4
+                        '5' -> 5
+                        '6' -> 6
+                        '7' -> 7
+                        '8' -> 8
+                        '9' -> 9
+                        _   -> 0
+                      
+parseHutton = buildExprParser [[ Infix (string "+" $> Add) AssocRight ]] decimal
